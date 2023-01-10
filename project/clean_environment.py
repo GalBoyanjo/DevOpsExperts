94eb2bb05e7cc63012bc9f815c5c7ee8ff3b6e6a
GalBoyanjo <galboyanjo@gmail.com>
2023-01-10 19:19:44 +0200

    Add headers for Cleanup env requests

--- project/clean_environment.py
+++ project/clean_environment.py
@@ -1,10 +1,14 @@
 import requests
 
-headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36'}
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0",
+    "Accept-Encoding": "*",
+    "Connection": "keep-alive"
+}
 
 try:
     requests.get('http://127.0.0.1:5000/stop_server', headers=headers)
 
-    requests.get('http://127.0.0.1:5001/stop_server', headers=headers)
+    requests.get('http://127.0.0.1:5001/stop_server')
 except Exception as exce:
     raise Exception("Environment cleanup failed", exce)
