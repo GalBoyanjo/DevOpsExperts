f1b35073337d151aba0ca4a565a4307b36b2284d
GalBoyanjo <galboyanjo@gmail.com>
2023-01-10 22:01:53 +0200

    Test on pipeline

--- project/clean_environment.py
+++ project/clean_environment.py
@@ -1,12 +1,14 @@
 import requests
 
 
+
+try:
+    requests.get('http://127.0.0.1:5001/stop_server')
+except Exception as web_exce:
+    raise Exception("web_app cleanup failed", web_exce)
+
 try:
     requests.get('http://127.0.0.1:5000/stop_server')
 
 except Exception as rest_exce:
     raise Exception("rest_app cleanup failed", rest_exce)
-try:
-    requests.get('http://127.0.0.1:5001/stop_server')
-except Exception as web_exce:
-    raise Exception("web_app cleanup failed", web_exce)
