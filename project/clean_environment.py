e2f9579bc511fc8f7c3afd954bdf1e51d61a5cfd
GalBoyanjo <galboyanjo@gmail.com>
2023-01-10 19:04:35 +0200

    Add headers for Cleanup env requests

--- project/clean_environment.py
+++ project/clean_environment.py
@@ -1,8 +1,10 @@
 import requests
 
-try:
-    requests.get('http://127.0.0.1:5000/stop_server')
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36'}
 
-    requests.get('http://127.0.0.1:5001/stop_server')
+try:
+    requests.get('http://127.0.0.1:5000/stop_server', headers=headers)
+
+    requests.get('http://127.0.0.1:5001/stop_server', headers=headers)
 except Exception as exce:
     raise Exception("Environment cleanup failed", exce)
