aa8ab290f6ffc38352082d3df86615ef1739b6de
GalBoyanjo <galboyanjo@gmail.com>
2023-01-10 14:05:34 +0200

    Project - second part(minor fix)

--- project/clean_environment.py
+++ project/clean_environment.py
@@ -5,4 +5,4 @@ try:
 
     requests.get('http://127.0.0.1:5001/stop_server')
 except Exception as exce:
-    raise Exception("Environment cleanup faild", exce)
+    raise Exception("Environment cleanup failed", exce)
