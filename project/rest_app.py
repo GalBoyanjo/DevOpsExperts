77229f0aaa1a0321ad3e1bc508b6cf578a47e7c7
GalBoyanjo <galboyanjo@gmail.com>
2023-01-10 21:25:39 +0200

    Test on pipeline

--- project/rest_app.py
+++ project/rest_app.py
@@ -1,9 +1,11 @@
+import os
+import signal
+
 from flask import Flask, request
 
 from db_connector import add_user, get_user, update_user, delete_user
 
-import os
-import signal
+
 
 app = Flask(__name__)
 
