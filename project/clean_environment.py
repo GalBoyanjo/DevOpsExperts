e12790739092501367439b7b6f62117472eebebd
GalBoyanjo <galboyanjo@gmail.com>
2023-01-11 14:51:10 +0200

    Test on pipeline

--- project/clean_environment.py
+++ project/clean_environment.py
@@ -2,8 +2,13 @@ import requests
 
 
 try:
-    requests.get('http://127.0.0.1:5000/stop_server')
-
-    requests.get('http://127.0.0.1:5001/stop_server')
+    print('5000 before')
+    rest_req = requests.get('http://127.0.0.1:5000/stop_server')
+    print('5000 after')
+    print(rest_req.json())
+    print('5001 before')
+    web_req = requests.get('http://127.0.0.1:5001/stop_server')
+    print('5001 after')
+    print(web_req.json())
 except Exception as exce:
     raise Exception("Environment cleanup failed", exce)
